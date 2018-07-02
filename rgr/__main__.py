7d1694f516a22acaea96d47937c144da30713155
Faxn <blake.curry@gmx.us>
2018-07-01 20:54:28 -0400

    aded roll from command line and an option flag to go to interactive prompt.

--- rgr/__main__.py
+++ rgr/__main__.py
@@ -1,6 +1,7 @@
 import sys
 import readline
 import traceback
+import argparse
 
 from . import lexer
 from .parseRoll import parser
@@ -9,23 +10,36 @@ if (sys.version_info.major < 3):
     print("RGR requires at least python 3.")
     sys.exit(1)
 
+ap = argparse.ArgumentParser(description='roll dice.')
+ap.add_argument('--interactive', '-i', action='store_true', help='run in interactive mode')
+ap.add_argument('roll', default='1d6', nargs=argparse.REMAINDER, help='a roll to evaluate now.')
 
-buff=""
-try:
-    while True:
-        line = input("RGR>").strip()
-        if( line == "quit" ):
-            sys.exit()
-        if( line == ""):
-            line=buff
-        try:
-            roller = parser.parse(line)
-            result, history = roller.roll()
-            print(result)
-            print(history)
-        except Exception as E:
-            traceback.print_exc()
-        buff=line
-        
-except EOFError:
-    print()
+args = ap.parse_args()
+
+
+
+if args.interactive:
+    buff=""
+    try:
+        while True:
+            line = input("RGR>").strip()
+            if( line == "quit" ):
+                sys.exit()
+            if( line == ""):
+                line=buff
+            try:
+                roller = parser.parse(line)
+                result, history = roller.roll()
+                print(result)
+                print(history)
+            except Exception as E:
+                traceback.print_exc()
+            buff=line
+            
+    except EOFError:
+        print()
+else:
+    roller = parser.parse(' '.join(args.roll))
+    result, history = roller.roll()
+    print(result)
+    print(history)
