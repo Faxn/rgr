8ec00a4d7e127ea0ec2e18fc310c5c1c9cc4782f
Blake Curry <blake.curry@gmx.us>
2019-03-30 19:40:48 -0400

    Use config to restore auto roll feature.

--- rgr/rgrcog.py
+++ rgr/rgrcog.py
@@ -4,7 +4,7 @@ import logging
 import os
 
 from redbot.core import commands
-from redbot.core imort Config
+from redbot.core import Config
 import rgr
 
 logger = logging.getLogger(__name__)
@@ -13,27 +13,8 @@ class RGR (commands.Cog):
         
     def __init__(self, bot):
         self.bot = bot
-        self.path = os.path.join("data", "rgrcog")
-        os.makedirs(self.path, exist_ok=True)
-        self.config_path = os.path.join(self.path, "config.json")
-        self._load_config()
-        self._save_config()
-        
-    def _load_config(self):
-        "Loads the config from the file to 'self.config' ."
-        self.config = {'listen_channels':{}}
-        if os.path.exists(self.config_path):
-            with open(self.config_path, 'r') as config_fp:
-                json_config = json.load(config_fp)
-            # collections.ChainMap is the normal Way to do this, but json.dump can't handle it (python 3.6.3).
-            self.config.update(json_config)
-    
-    def _save_config(self):
-        "Write out 'self.config' to the config file."
-        with open(self.config_path, 'w') as config_fp:
-            json.dump(self.config, config_fp, indent=4)
-    
-    
+        self.config = Config.get_conf(self, identifier=15646486532)
+        self.config.register_global(listen_channels={})
     
     # Parse !rgr verbiage
     @commands.command(aliases=["r", "roll", "dice"], pass_context=True, rest_is_raw=True, description='Rolls dice. Uses an in development domain specific language.')
@@ -51,21 +32,24 @@ class RGR (commands.Cog):
     
     @commands.command(aliases=["rollChannel", "autoRoll"], pass_context=True, rest_is_raw=True, description='Sets the current channel roll status')
     @asyncio.coroutine
-    async def set_auto_roll(self, ctx, enabled: bool=True):    
-        self.config['listen_channels'][ctx.message.channel.id] = enabled
-        self._save_config()
+    async def set_auto_roll(self, ctx, enabled: bool=True):
+        async with self.config.listen_channels() as lc:
+            lc[ctx.message.channel.id] = enabled
     
     @asyncio.coroutine
     async def on_message(self, message):
         if message.author.id == self.bot.user.id:
             return
-        if self.config['listen_channels'][message.channel.id]: 
-            try:
-                logger.debug("Rolling: %s", message.content)
-                _, response, _ = rgr.roll(message.content)
-                await self.bot.send_message(message.channel,  "{} rol {}".format(message.author, response))
-            except Exception as e:
-                logger.debug("Problem while rolling: %s",e)
+        cid = str(message.channel.id)
+        async with self.config.listen_channels() as lc:
+            if (cid not in lc) or (not lc[cid]):
+                return
+        try:
+            logger.debug("Rolling: %s", message.content)
+            _, response, _ = rgr.roll(message.content)
+            await message.channel.send(  "{} rol {}".format(message.author, response))
+        except Exception as e:
+            logger.debug("Problem while rolling: %s",e)
 
 
 def setup(bot):
